a02bc62eee23a99b8cb8c0156435b1a230df257f
Sara <saraalmohnds0@gmail.com>
2024-04-25 02:47:07 +0200

    tree screen finished as front and back end

--- app/src/main/java/com/example/ourproject/FrontEnd/AppNavGraph.kt
+++ app/src/main/java/com/example/ourproject/FrontEnd/AppNavGraph.kt
@@ -54,6 +54,7 @@ fun appNavGraph(navController: NavHostController) {
             responseImages(navController,requestId.toString(),requestType.toString())
         }
         composable(route = ScreensRoute.Levels.route) { levels(navController) }
+        composable(route = ScreensRoute.Plant.route) { TreeAnimation() }
 
     }
 }
